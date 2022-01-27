3582c2b42a6dcda0d152fffc7cc9f5b3096e9697
Michael McGreal <mcgrealife@gmail.com>
2022-01-27 12:11:46 -0500

    IT WAS A Z-INDEX PROBLEM!!!!! The phone frame was blocking access to scroll the container. Wow. hallelujah

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,7 @@ export default function Card() {
   return (
     // shadow-card should work 
     // w-[219.1px]
-    <div className={`w-[219.1px] h-[68px] rounded-lg snap-center bg-white shadow-lg`} >
+    <div className={`w-[219.1px] h-[68px] rounded-lg snap-center bg-white shadow-lg grid`} >
     </div >
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,19 @@ export default function Home() {
         <div className='grow'>
           <p>title</p>
           <div className='grid grid-cols-[9.5px_255px_9.5px] grid-rows-[8px_552.2px_8.1px]  justify-center'>
+
             <img src="/map-1.png" alt="map" className='row-start-2 row-span-4 col-start-2 col-span-2 pt-[101px] z-2' />
 
             <img src="/status-search-filter.svg" alt="status"
               className='row-start-2 row-span-4 col-start-2 col-span-2 z-2 shadow-lg' />
 
-            <div className='row-start-2 col-start-2 rounded-2xl z-4 pb-[28.5px]'>
-              <div className='flex flex-row'>
+            <div className='row-start-2 col-start-2 self-end z-6 pb-[28.5px] overflow-x-scroll scrollbar-hide snap-x'>
+
+              <div className='flex flex-row min-w-max gap-2 px-4 '>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
                 <Card />
                 <Card />
                 <Card />
@@ -41,7 +47,7 @@ export default function Home() {
 
             <img
               src='/frame-hollow.svg'
-              className='row-start-1 row-span-3 col-start-1 col-span-3 z-6'
+              className='row-start-1 row-span-3 col-start-1 col-span-3 z-5'
             />
 
           </div>
