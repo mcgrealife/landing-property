a69490c23a71a593496473d610caeb74dd4e055f
Michael McGreal <mcgrealife@gmail.com>
2022-01-28 12:34:14 -0500

    fixed classes in marker text style

--- components/Marker.js
+++ components/Marker.js
@@ -15,17 +15,32 @@ export default function Marker({ text, state }) {
     }
   }
 
+  const textColor = () => {
+    let resultString = ''
+    switch (state) {
+      case 'selected':
+        resultString = 'text-white'
+        break
+      case 'viewed':
+        resultString = 'text-[rgba(60, 64, 67, 0.5)]'
+        break
+      case 'unviewed':
+        resultString = 'text-[rgba(51,51,51,1)]'
+        break
+      default:
+        resultString = ''
+    }
+    return resultString
+  }
 
-  // ${state == 'selected' ? 'white' : '[rgba(60,64,67,0.5)]'}
 
-  // ${state == 'selected' ? '[rgba(54,108,165,1)]' : 'white'}
+  const className = `${textColor} + text-[7.85px] font-[600] text-center leading-[8px]`
 
-  // text - [rgba(60, 64, 67, 0.5)]
 
   return (
     <div className='flex flex-col drop-shadow-[0_0.654028px_2.61611px_rgba(60,64,67,0.2)]'>
-      <div className={` px-[3.92px]  pt-[3.27px] pb-[2.62px] grid place-content-center bg-${state == 'selected' ? '[rgba(54,108,165,1)]' : 'white'} rounded-[2.62px]`}>
-        <p className={`text-[7.85px] text-${state == 'selected' ? 'white' : '[rgba(60,64,67,0.5)]'} font-[600] text-center leading-[8px]`}>{text}</p>
+      <div className={` px-[3.92px]  pt-[3.27px] pb-[2.62px] grid place-content-center ${state == 'selected' ? 'bg-[rgba(54,108,165,1)]' : 'bg-white'} rounded-[2.62px]`}>
+        <p className={`text-[7.85px] ${state == 'selected' ? 'text-white' : state == 'viewed' ? 'text-[rgba(60,64,67,0.5)]' : 'text-[rgba(51,51,51,1)]'}  font-[600] text-center leading-[8px]`}>{text}</p>
       </div>
       <div className={`${state == 'selected' ? 'arrowDownBlue' : 'arrowDownWhite'} self-center`} />
     </div>
