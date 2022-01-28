6e46a492d2f7bc82d4bdbd8f5e107542d0b58cc5
Michael McGreal <mcgrealife@gmail.com>
2022-01-27 21:14:30 -0500

    to make the cards swipe in a fixed duration within a scrollTrigger, I ustilized the .to tweens callbacks! onStart and onReverseComplete

--- pages/index.js
+++ pages/index.js
@@ -24,21 +24,47 @@ export default function Home() {
     console.log('completed')
   }
 
+  const reverseUpdate = () => {
+    setMarkerStyle('unviewed')
+    console.log('uncomplete')
+  }
+
   let tl = useRef()
 
   useEffect(() => {
     tl.current = gsap.timeline({
       scrollTrigger: {
         trigger: frameContainer.current,
+        end: "+=6000",
         pin: true,
-        scrub: 2
+        scrub: 1,
+        anticipatePin: 1,
       }, paused: true
     })
-      .to(cards.current, {
-        x: -460,
-        onComplete: update
-      })
-      .to(floatingText1.current, { y: -1000 }, "<1%")
+      // .to(cards.current, {
+      //   x: -460,
+      //   duration: 1,
+      //   onComplete: update,
+      //   onReverseComplete: reverseUpdate,
+
+      // })
+      .to(floatingText1.current, {
+        y: -1000,
+        onStart: () => {
+          gsap.to(cards.current, {
+            x: -460,
+            duration: 1,
+            onStart: update
+          })
+        },
+        onReverseComplete: () => {
+          gsap.to(cards.current, {
+            x: '+=460',
+            onStart: reverseUpdate
+          })
+        }
+
+      }, "<1%")
 
   }, [])
 
@@ -54,69 +80,67 @@ export default function Home() {
 
       <div ref={frameContainer} className='flex flex-col min-h-screen font-[gilroy]'>
 
-        <div className='grow'>
-
-          <div className='grid grid-cols-[9.5px_255px_9.5px] grid-rows-[8px_552.2px_8.1px] ml-[24px] sticky'>
-            <div className='row-start-2 col-start-2 z-4 place-self-center'>
-              <Marker
-                text={markerStyle}
-                state='selected'
-              />
-
-              <Marker
-                text='$3000'
-                state='viewed'
-              />
-
-              <Marker
-                text='$2700'
-                state='unviewed'
-              />
-
-            </div>
+        <div className='grid grid-cols-[9.5px_255px_9.5px] grid-rows-[8px_552.2px_8.1px] ml-[24px] sticky'>
 
 
-            <img src="/map-1.png" alt="map" className='row-start-2 row-span-4 col-start-2 col-span-2 pt-[101px] z-2' />
-
-            <img src="/status-search-filter.svg" alt="status"
-              className='row-start-2 row-span-4 col-start-2 col-span-2 z-2 shadow-lg' />
-
-            <div className='row-start-2 col-start-2 self-end z-6 pb-[28.5px] overflow-x-scroll scrollbar-hide snap-x  pt-2'>
-
-              <div ref={cards} className='flex flex-row min-w-max gap-2 px-4 overflow-visible'>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-              </div>
-
-            </div>
-
-
-            <img src="/home.svg" alt="home" className='row-start-2 col-start-2 justify-self-center self-end pb-[5.23px] z-20' />
-            {/* shadow-frame should work tailwing.conifig */}
-
-            <img
-              src='/frame-hollow.svg'
-              className='row-start-1 row-span-3 col-start-1 col-span-3 z-5'
+          <div className='row-start-2 col-start-2 z-4 place-self-center'>
+            <Marker
+              text={markerStyle}
+              state='selected'
             />
+
+            <Marker
+              text='$3000'
+              state='viewed'
+            />
+
+            <Marker
+              text='$2700'
+              state='unviewed'
+            />
+
           </div>
 
 
+          <img src="/map-1.png" alt="map" className='row-start-2 row-span-4 col-start-2 col-span-2 pt-[101px] z-2' />
 
-          <div ref={scheduleTour} className='bg-white shadow-[0_-2px_4px_rgba(60,64,67,0.1)]  p-[8px] w-full grid fixed bottom-0 z-20'>
-            <button className='bg-resider-blue-primary  rounded text-white p-2 shadow-[0_1px_2px_rgba(60,64,67,0.3)] align-center font-[500] text-[14px] leading-[18px] tracking-[0.2px] px-[32px] py-[15px]'>Schedule Demo</button>
+          <img src="/status-search-filter.svg" alt="status"
+            className='row-start-2 row-span-4 col-start-2 col-span-2 z-2 shadow-lg' />
+
+          <div className='row-start-2 col-start-2 self-end z-6 pb-[28.5px] overflow-x-scroll scrollbar-hide snap-x  pt-2'>
+
+            <div ref={cards} className='flex flex-row min-w-max gap-2 px-4 overflow-visible'>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </div>
+
           </div>
 
+
+          <img src="/home.svg" alt="home" className='row-start-2 col-start-2 justify-self-center self-end pb-[5.23px] z-20' />
+          {/* shadow-frame should work tailwing.conifig */}
+
+          <img
+            src='/frame-hollow.svg'
+            className='row-start-1 row-span-3 col-start-1 col-span-3 z-5'
+          />
         </div>
+
+
+
+
+
+
 
         <div ref={floatingText1} className='grid bg-white  rounded shadow-[0_1px_6px_rgba(60,64,67,0.24)] h-fit w-fit self-end mr-[12px]'>
           <div className='py-[32px] px-[24px] flex flex-col gap-[16px] rounded-[12px]'>
@@ -125,16 +149,10 @@ export default function Home() {
           </div>
         </div>
 
-
-
-
-
-
-
-
-
       </div>
-
+      <div ref={scheduleTour} className='bg-white shadow-[0_-2px_4px_rgba(60,64,67,0.1)]  p-[8px] w-full grid fixed bottom-0 z-20'>
+        <button className='bg-resider-blue-primary  rounded text-white p-2 shadow-[0_1px_2px_rgba(60,64,67,0.3)] align-center font-[500] text-[14px] leading-[18px] tracking-[0.2px] px-[32px] py-[15px]'>Schedule Demo</button>
+      </div>
     </>
 
   )
