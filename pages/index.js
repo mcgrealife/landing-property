38877b1e3947564a45224ad40cbb5b99746ce3de
Michael McGreal <mcgrealife@gmail.com>
2022-02-20 06:59:55 -0500

    Revert "before moving tweens to variables with dynamic values"
    This reverts commit 9974719d5813b44feff11d82f7945736ee4891d5.

--- pages/index.js
+++ pages/index.js
@@ -521,11 +521,15 @@ export default function Home() {
 
       <div id='hero-section' ref={heroSection} className='flex flex-col place-items-center text-center'>
 
-        <img src="/logo-square.svg" alt="logo-square" className='h-[36px] lg:h-[52px] mt-[28px] lg:mt-[12px]' />
+        <img src="/logo-square-mobile.svg" alt="logo-square-mobile" className='block lg:hidden  mt-[28px]' />
+        {/* h-[36px] */}
+
+        <img src="/logo-square-desktop.svg" alt="logo-square-desktop" className='hidden lg:block  mt-[12px]' />
+        {/* h-[52px] */}
 
         <h1 className='mt-[24.49px] lg:mt-[24px] text-[rgba(60,64,67,1)] font-[700] text-[36px] lg:text-[72px] leading-[48px] lg:leading-[84px] tracking-[0.1px] max-w-[326px] lg:max-w-[639px]'>A <span className='text-[rgba(54,108,165,1)]'>better</span> way to generate leads</h1>
 
-        <p ref={headerSubText} className='text-[rgba(96,99,103,1)] mt-[15.15px] lg:mt-[16px] text-[18px] lg:text-[26px] leading-[30px] lg:leading-[38px] max-w-[326px] lg:max-w-[579px]'>Resider is a smart, efficient and helpful way to qualify and schedule your prospective tenants.</p>
+        <p ref={headerSubText} className='text-[rgba(96,99,103,1)] mt-[15.51px] lg:mt-[16px] font-[500] text-[18px] lg:text-[26px] leading-[30px] lg:leading-[38px] max-w-[326px] lg:max-w-[579px] mb-[24.9px] lg:mb-[24px]'>Resider is a smart, efficient and helpful way to qualify and schedule your prospective tenants.</p>
 
         <div id="phone-hero" className='grid justify-items-center justify-center rounded-br-full rounded-bl-full overflow-hidden'>
 
